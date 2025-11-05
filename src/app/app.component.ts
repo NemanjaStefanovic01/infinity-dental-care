0e18d645245700775fd94760dfbae4db1e6cba81
MuseGt <nemanja.stefanovic3105@gmail.com>
2025-11-05 10:07:45 +0100

    Started working on home page, with adding video to landing section and integrating nav bar into that design.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    NavBarComponent
+    NavBarComponent,
+    RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UslugeComponent } from './pages/usluge/usluge.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/pocetna', pathMatch: 'full'},
+    { path: 'pocetna', component: HomeComponent},
+    { path: 'usluge', component: UslugeComponent}
+];
