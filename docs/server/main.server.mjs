ed7cdc39cf907bbffcbecec16d20082bcbba8fd2
MuseGt <nemanja.stefanovic3105@gmail.com>
2025-11-24 08:54:22 +0100

    Fixed breaking title for most of mobile sizes, and modified the about us text. Push with updated Build.

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-LXXIYSAG.mjs";import"./chunk-EP7Z5ME3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-2KJNJUOS.mjs";import"./chunk-EP7Z5ME3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
